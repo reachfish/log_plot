2f0436ab52588cf04ce9994ec94af27f8629834f
yujun <yu.jun.reach@gmail.com>
2018-01-21 15:55:52 +0800

    加入命令行解析

--- plot.py
+++ plot.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pattern
 import numpy
+import getopt
 
 def show_plot(fields, log_files, save_name, incl=None, begin_time=None, end_time=None):
 	mgr = pattern.PatternManager()
@@ -36,6 +37,7 @@ def show_plot(fields, log_files, save_name, incl=None, begin_time=None, end_time
 
 if __name__ == "__main__":
 	import sys
+	import re
 	import config
 	mgr = pattern.PatternManager()
 	mgr.load_patterns(config.patterns)
@@ -48,8 +50,18 @@ if __name__ == "__main__":
 		print """
 usage:
 
-log_file_name     out_pic_name     fields
+-f log_file_names  [-o out_pic_name]  -a  fields   [-i  include]   [-b  begin_time]  [-e  end_time]
 	查看指标图
+
+	-f log_file_names  log文件名,可以多个，文件名间用逗号分开
+	-o out_pic_name 输出图片名，默认为out
+	-a 指标，指标间用逗号分开
+	-i include    日志需要包含的内容，可选
+	-b begin_time 日志开始时间，格式为 2000-01-01 00:00:00 这样的，可选
+	-e end_time   日志结束时间，可选
+
+	python plot.py -f 1.log  -out video_rtt  -a   video_rtt
+	python plot.py -f 1.log  -a video_rtt,video_decode  -b "2018-01-20 10:00:00"
 
 support fields:
 
@@ -60,20 +72,46 @@ support fields:
 		for i, fields in config.cases.iteritems():
 			print i, "  ".join(fields)
 
-	if len(sys.argv) < 4:
+	try: 
+		opts,args = getopt.getopt(sys.argv[1:], "f:o:a:i:b:e", [])
+	except getopt.GetoptError:
 		help()
 		exit()
 
-	log_file_names = sys.argv[1]
-	save_name = sys.argv[2]
-	fields = sys.argv[3:]
-	index = sys.argv[3]
-	if index.isdigit():
-		fields = config.cases.get(int(index), None)
-		if not fields:
-			help()
-			exit()
+	log_file_names = ""
+	out_pic_name = "out"
+	fields = []
+	begin_time = ""
+	end_time = ""
+	include = ""
 
-	show_plot(fields, log_file_names, save_name)
+	p = re.compile(r"[;,\s]\s*")
+	for o, a in opts:
+		if not a: continue
+		if o == "-f":
+			log_file_names = p.split(a)
+		elif o == "-o":
+			out_pic_name = a
+		elif o == "-i":
+			include = a
+		elif o == "-b":
+			begin_time = a
+		elif o == "-e":
+			end_time = a
+		elif o == "-a":
+			for v in p.split(a):
+				if v.isdigit():
+					fields.extend(config.cases.get(int(v), []))
+				else:
+					fields.append(v)
 
+	if not log_file_names:
+		print "请输入文件名 -f"
+		exit()
+
+	if not fields:
+		print "请输入查看指标 -a"
+		exit()
+
+	show_plot(fields, log_file_names, out_pic_name, include, begin_time, end_time)
 
